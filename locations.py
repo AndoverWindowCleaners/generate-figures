58d7d4d76aa2122e7bde11ff4aa93516d947d3fc
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-10-08 20:32:30 -0400

    updated centers

--- locations.py
+++ locations.py
@@ -11,18 +11,31 @@ xs, ys = [], []
 for folder in os.listdir(labels_folder):
     if folder != '.DS_Store':
         for filename in os.listdir(f"{labels_folder}{folder}"):
+            max_imgs = 0
+            x_best = []
+            y_best = []
             if filename.endswith("YOLO"):
                 for _file in os.listdir(f"{labels_folder}{folder}/{filename}"):
                     with open(f"{labels_folder}{folder}/{filename}/{_file}") as f:
-                        lines = []
+                        num_lines = 0
+                        x_temps = []
+                        y_temps = []
                         for line in f:
+                            num_lines += 1
                             line = line.split()
                             label, rest = int(line[0]), line[1:]
                             xcenter, ycenter, w, h = map(float, rest)
                             # lines.append([int((xcenter-w/2)*WIDTH), int((ycenter-h/2)*HEIGHT), int((xcenter+w/2)*WIDTH), int((ycenter-h/2)*HEIGHT)])
-                            xs.append(int(xcenter*WIDTH))
-                            ys.append(int(ycenter*HEIGHT))
-                    break
+                            x_temps.append(int(xcenter*WIDTH))
+                            y_temps.append(int(ycenter*HEIGHT))
+                        if num_lines > max_imgs:
+                            max_imgs = num_lines
+                            x_best = x_temps.copy()
+                            y_best = y_temps.copy()
+            xs += x_best
+            ys += y_best
+
+print(len(xs))
 
 plt.scatter(xs, ys, c='b')
 
